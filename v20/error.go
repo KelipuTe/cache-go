b15256cef36c64ac2139306149dba36c4d87aa88
西柊慧音 <xhy_1365@sina.com>
2022-12-14 21:44:44 +0800

    review, local, memory limit

--- v20/error.go
+++ v20/error.go
@@ -3,5 +3,5 @@ package v20
 import "errors"
 
 var (
-	errKeyNotFound = errors.New("cache: key not found")
+	errKeyNotFound = errors.New("f8GetCache: key not found")
 )
--- v20/memory_limit_test.go
+++ v20/memory_limit_test.go
@@ -3,7 +3,6 @@ package v20
 import (
 	"context"
 	"github.com/stretchr/testify/assert"
-	"sync"
 	"testing"
 	"time"
 )
@@ -11,7 +10,7 @@ import (
 func TestS6CacheWithMemoryLimitF8Set(p7s6t *testing.T) {
 	s5s6case := []struct {
 		name          string
-		cache         func() *S6CacheWithMemoryLimit
+		f8GetCache    func() *S6CacheWithMemoryLimit
 		key           string
 		value         string
 		wantNowMemory int64
@@ -20,14 +19,10 @@ func TestS6CacheWithMemoryLimitF8Set(p7s6t *testing.T) {
 	}{
 		{
 			name: "key_not_exist",
-			cache: func() *S6CacheWithMemoryLimit {
-				return &S6CacheWithMemoryLimit{
-					i9Cache:   F8NewS6LocalForTest(),
-					NowMemory: 0,
-					MaxMemory: 16,
-					s5LRU:     make([]string, 0),
-					p7s6lock:  &sync.Mutex{},
-				}
+			f8GetCache: func() *S6CacheWithMemoryLimit {
+				i9cache := F8NewS6LocalForTest()
+				p7s6cache := F8NewS6CacheWithMemoryLimitForTest(i9cache, 16)
+				return p7s6cache
 			},
 			key:           "key1",
 			value:         "aaaa",
@@ -37,19 +32,13 @@ func TestS6CacheWithMemoryLimitF8Set(p7s6t *testing.T) {
 		},
 		{
 			name: "key_exist",
-			cache: func() *S6CacheWithMemoryLimit {
-				return &S6CacheWithMemoryLimit{
-					i9Cache: &S6Local{
-						m3data: map[string]*s6Unit{
-							"key1": &s6Unit{value: "aaaa", deadline: time.Time{}},
-						},
-						p7s6lock: &sync.RWMutex{},
-					},
-					NowMemory: 4,
-					MaxMemory: 16,
-					s5LRU:     []string{"key1"},
-					p7s6lock:  &sync.Mutex{},
-				}
+			f8GetCache: func() *S6CacheWithMemoryLimit {
+				i9cache := F8NewS6LocalForTest()
+				i9cache.m3data["key1"] = &s6Unit{value: "aaaa", deadline: time.Time{}}
+				p7s6cache := F8NewS6CacheWithMemoryLimitForTest(i9cache, 16)
+				p7s6cache.NowMemory = 4
+				p7s6cache.s5LRU = []string{"key1"}
+				return p7s6cache
 			},
 			key:           "key1",
 			value:         "bbbbbb",
@@ -59,19 +48,13 @@ func TestS6CacheWithMemoryLimitF8Set(p7s6t *testing.T) {
 		},
 		{
 			name: "add_key",
-			cache: func() *S6CacheWithMemoryLimit {
-				return &S6CacheWithMemoryLimit{
-					i9Cache: &S6Local{
-						m3data: map[string]*s6Unit{
-							"key1": &s6Unit{value: "aaaa", deadline: time.Time{}},
-						},
-						p7s6lock: &sync.RWMutex{},
-					},
-					NowMemory: 4,
-					MaxMemory: 16,
-					s5LRU:     []string{"key1"},
-					p7s6lock:  &sync.Mutex{},
-				}
+			f8GetCache: func() *S6CacheWithMemoryLimit {
+				i9cache := F8NewS6LocalForTest()
+				i9cache.m3data["key1"] = &s6Unit{value: "aaaa", deadline: time.Time{}}
+				p7s6cache := F8NewS6CacheWithMemoryLimitForTest(i9cache, 16)
+				p7s6cache.NowMemory = 4
+				p7s6cache.s5LRU = []string{"key1"}
+				return p7s6cache
 			},
 			key:           "key2",
 			value:         "bbbb",
@@ -80,23 +63,17 @@ func TestS6CacheWithMemoryLimitF8Set(p7s6t *testing.T) {
 			wantErr:       nil,
 		},
 		{
-			name: "delete_key",
-			cache: func() *S6CacheWithMemoryLimit {
-				return &S6CacheWithMemoryLimit{
-					i9Cache: &S6Local{
-						m3data: map[string]*s6Unit{
-							"key1": &s6Unit{value: "aaaa", deadline: time.Time{}},
-							"key2": &s6Unit{value: "bbbb", deadline: time.Time{}},
-							"key3": &s6Unit{value: "cccc", deadline: time.Time{}},
-							"key4": &s6Unit{value: "dddd", deadline: time.Time{}},
-						},
-						p7s6lock: &sync.RWMutex{},
-					},
-					NowMemory: 16,
-					MaxMemory: 16,
-					s5LRU:     []string{"key1", "key2", "key3", "key4"},
-					p7s6lock:  &sync.Mutex{},
-				}
+			name: "add_delete_key",
+			f8GetCache: func() *S6CacheWithMemoryLimit {
+				i9cache := F8NewS6LocalForTest()
+				i9cache.m3data["key1"] = &s6Unit{value: "aaaa", deadline: time.Time{}}
+				i9cache.m3data["key2"] = &s6Unit{value: "bbbb", deadline: time.Time{}}
+				i9cache.m3data["key3"] = &s6Unit{value: "cccc", deadline: time.Time{}}
+				i9cache.m3data["key4"] = &s6Unit{value: "dddd", deadline: time.Time{}}
+				p7s6cache := F8NewS6CacheWithMemoryLimitForTest(i9cache, 16)
+				p7s6cache.NowMemory = 16
+				p7s6cache.s5LRU = []string{"key1", "key2", "key3", "key4"}
+				return p7s6cache
 			},
 			key:           "key5",
 			value:         "eeee",
@@ -105,23 +82,17 @@ func TestS6CacheWithMemoryLimitF8Set(p7s6t *testing.T) {
 			wantErr:       nil,
 		},
 		{
-			name: "delete_key_twice",
-			cache: func() *S6CacheWithMemoryLimit {
-				return &S6CacheWithMemoryLimit{
-					i9Cache: &S6Local{
-						m3data: map[string]*s6Unit{
-							"key1": &s6Unit{value: "aaaa", deadline: time.Time{}},
-							"key2": &s6Unit{value: "bbbb", deadline: time.Time{}},
-							"key3": &s6Unit{value: "cccc", deadline: time.Time{}},
-							"key4": &s6Unit{value: "dddd", deadline: time.Time{}},
-						},
-						p7s6lock: &sync.RWMutex{},
-					},
-					NowMemory: 16,
-					MaxMemory: 16,
-					s5LRU:     []string{"key1", "key2", "key3", "key4"},
-					p7s6lock:  &sync.Mutex{},
-				}
+			name: "add_delete_2key",
+			f8GetCache: func() *S6CacheWithMemoryLimit {
+				i9cache := F8NewS6LocalForTest()
+				i9cache.m3data["key1"] = &s6Unit{value: "aaaa", deadline: time.Time{}}
+				i9cache.m3data["key2"] = &s6Unit{value: "bbbb", deadline: time.Time{}}
+				i9cache.m3data["key3"] = &s6Unit{value: "cccc", deadline: time.Time{}}
+				i9cache.m3data["key4"] = &s6Unit{value: "dddd", deadline: time.Time{}}
+				p7s6cache := F8NewS6CacheWithMemoryLimitForTest(i9cache, 16)
+				p7s6cache.NowMemory = 16
+				p7s6cache.s5LRU = []string{"key1", "key2", "key3", "key4"}
+				return p7s6cache
 			},
 			key:           "key5",
 			value:         "eeeeeeee",
@@ -133,7 +104,7 @@ func TestS6CacheWithMemoryLimitF8Set(p7s6t *testing.T) {
 
 	for _, t4value := range s5s6case {
 		p7s6t.Run(t4value.name, func(p7s6t2 *testing.T) {
-			cache := t4value.cache()
+			cache := t4value.f8GetCache()
 			err := cache.F8Set(context.Background(), t4value.key, t4value.value, time.Minute)
 			assert.Equal(p7s6t2, t4value.wantErr, err)
 			if nil != err {
@@ -141,6 +112,74 @@ func TestS6CacheWithMemoryLimitF8Set(p7s6t *testing.T) {
 			}
 			assert.Equal(p7s6t2, t4value.wantNowMemory, cache.NowMemory)
 			assert.Equal(p7s6t2, t4value.wantS5LRU, cache.s5LRU)
+		})
+	}
+}
+
+func TestS6CacheWithMemoryLimitF8Delete(p7s6t *testing.T) {
+	s5s6case := []struct {
+		name          string
+		f8GetCache    func() *S6CacheWithMemoryLimit
+		key           string
+		wantNowMemory int64
+		wantS5LRU     []string
+		wantErr       error
+	}{
+		{
+			name: "key_not_exist",
+			f8GetCache: func() *S6CacheWithMemoryLimit {
+				i9cache := F8NewS6LocalForTest()
+				p7s6cache := F8NewS6CacheWithMemoryLimitForTest(i9cache, 16)
+				return p7s6cache
+			},
+			key:           "key1",
+			wantNowMemory: 0,
+			wantS5LRU:     []string{},
+			wantErr:       errKeyNotFound,
+		},
+		{
+			name: "key_exist",
+			f8GetCache: func() *S6CacheWithMemoryLimit {
+				i9cache := F8NewS6LocalForTest()
+				i9cache.m3data["key1"] = &s6Unit{value: "aaaa", deadline: time.Time{}}
+				p7s6cache := F8NewS6CacheWithMemoryLimitForTest(i9cache, 16)
+				p7s6cache.NowMemory = 4
+				p7s6cache.s5LRU = []string{"key1"}
+				return p7s6cache
+			},
+			key:           "key1",
+			wantNowMemory: 0,
+			wantS5LRU:     []string{},
+			wantErr:       nil,
+		},
+		{
+			name: "delete_key",
+			f8GetCache: func() *S6CacheWithMemoryLimit {
+				i9cache := F8NewS6LocalForTest()
+				i9cache.m3data["key1"] = &s6Unit{value: "aaaa", deadline: time.Time{}}
+				i9cache.m3data["key2"] = &s6Unit{value: "bbbb", deadline: time.Time{}}
+				p7s6cache := F8NewS6CacheWithMemoryLimitForTest(i9cache, 16)
+				p7s6cache.NowMemory = 8
+				p7s6cache.s5LRU = []string{"key1", "key2"}
+				return p7s6cache
+			},
+			key:           "key1",
+			wantNowMemory: 4,
+			wantS5LRU:     []string{"key2"},
+			wantErr:       nil,
+		},
+	}
+
+	for _, t4value := range s5s6case {
+		p7s6t.Run(t4value.name, func(p7s6t2 *testing.T) {
+			p7s6cache := t4value.f8GetCache()
+			err := p7s6cache.F8Delete(context.Background(), t4value.key)
+			assert.Equal(p7s6t2, t4value.wantErr, err)
+			if nil != err {
+				return
+			}
+			assert.Equal(p7s6t2, t4value.wantNowMemory, p7s6cache.NowMemory)
+			assert.Equal(p7s6t2, t4value.wantS5LRU, p7s6cache.s5LRU)
 		})
 	}
 }
